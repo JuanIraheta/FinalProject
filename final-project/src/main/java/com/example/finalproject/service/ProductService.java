d685edb30bafe2940f81d73830c9714e7ab609ad
JuanIraheta <jiraheta@applaudostudios.dev>
2022-07-21 16:56:03 -0600

    Feature #Adding the product service

---
+++ final-project/src/main/java/com/example/finalproject/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.example.finalproject.service;
+
+import com.example.finalproject.persistence.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+}
