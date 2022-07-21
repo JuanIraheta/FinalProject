a8a88a1644e8688b52f3bb9d6b28af35528c1e1e
JuanIraheta <jiraheta@applaudostudios.dev>
2022-07-21 16:50:32 -0600

    Feature #Adding The Controller Layer

---
+++ final-project/src/main/java/com/example/finalproject/web/controllers/ProductsController.java
@@ -0,0 +1,9 @@
+package com.example.finalproject.web.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductsController {
+}
