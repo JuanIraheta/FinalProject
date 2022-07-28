a68848355cf11d318b9cf5f05958b121fda0cd38
JuanIraheta <jiraheta@applaudostudios.dev>
2022-07-27 18:40:53 -0600

    Refactor #Refactoring Dtos validation

--- final-project/src/main/java/com/example/finalproject/web/DTO/CreatePaymentMethodDTO.java
+++ final-project/src/main/java/com/example/finalproject/web/DTO/CreatePaymentMethodDTO.java
@@ -4,16 +4,24 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+
 @Getter
 @Setter
 @Builder
 public class CreatePaymentMethodDTO {
 //Dto used to create a payment method on the checkout
 
+
+    @NotBlank(message = "Name is Mandatory")
     private String name;
 
+    @PositiveOrZero(message = "Founds must be positive or zero")
     private double founds;
 
+    @NotBlank(message = "Payment type is mandatory")
     private String paymentType;
 
 }
