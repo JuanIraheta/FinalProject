8f403b081502c879f66b87162355f1208fca19b7
JuanIraheta <jiraheta@applaudostudios.dev>
2022-07-30 12:18:54 -0600

    Test #OrderController test

--- final-project/src/main/java/com/example/finalproject/web/controllers/OrderController.java
+++ final-project/src/main/java/com/example/finalproject/web/controllers/OrderController.java
@@ -3,10 +3,12 @@ package com.example.finalproject.web.controllers;
 import com.example.finalproject.service.implementation.OrderServiceImplementation;
 import com.example.finalproject.web.DTO.OrderDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,12 +20,14 @@ public class OrderController {
     private final OrderServiceImplementation orderServiceImplementation;
 
     @GetMapping(value = "/API/users/orders")
+    @ResponseStatus(HttpStatus.OK)
     public List<OrderDTO> getAllOrders(@AuthenticationPrincipal Jwt principal)
     {
         return orderServiceImplementation.getAllOrders(getEmailByPrincipal(principal));
     }
 
     @GetMapping(value = "/API/users/orders/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public OrderDTO getOrder(@AuthenticationPrincipal Jwt principal,@PathVariable long id)
     {
         return orderServiceImplementation.getOrder(getEmailByPrincipal(principal),id);
--- final-project/src/main/java/com/example/finalproject/web/controllers/ProductsController.java
+++ final-project/src/main/java/com/example/finalproject/web/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,11 +19,13 @@ public class ProductsController {
     private final ProductServiceImplementation productServiceImplementation;
 
     @GetMapping(value = "/API/products")
+    @ResponseStatus(HttpStatus.OK)
     public List<ProductDTO> getProducts()
     {
         return productServiceImplementation.getAllProducts();
     }
     @GetMapping(value = "/API/products/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ProductDTO getProduct(@PathVariable long id)
     {
         return productServiceImplementation.getProduct(id);
